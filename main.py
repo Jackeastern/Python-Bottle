2d9bd310520aebded39e632ecad3473f90bc8804
Jacques <jacques@jawisoftbcd.com>
2023-01-13 22:31:35 +0200

    Python Bottle Get & Post

--- main.py
+++ main.py
@@ -21,4 +21,24 @@ def todo_list():
     return output
 
 
+@route('/new', method='GET')
+def new_item():
+
+    if request.GET.save:
+
+        new = request.GET.task.strip()
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+
+        c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new, 1))
+        new_id = c.lastrowid
+
+        conn.commit()
+        c.close()
+
+        return '<p>The new task was inserted into the database, the ID is %s</p>' % new_id
+    else:
+        return template('new_task.tpl')
+
+
 run(host='localhost', port=8080, debug=True, reloader=True)
